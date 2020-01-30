d4810745ce839d3610ebb6233c52ed51120df9ec
CyrilleKolle <ngidekollecyrille@gmail.com>
2020-01-30 20:42:43 +0100

    second

--- labb3.ObjectiveC.Cyrille.Ngide.Kolle/ViewController.h
+++ labb3.ObjectiveC.Cyrille.Ngide.Kolle/ViewController.h
@@ -7,8 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Todo.h"
+#import "ToDOList.h"
+#import "SingletonClass.h"
 
-@interface ViewController : UIViewController
+
+@interface ViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
+
+@property (nonatomic) IBOutlet UITableView* tableV;
+@property (nonatomic) ToDOList* todolist;
+@property (nonatomic) Todo* todo;
+@property (nonatomic) NSMutableArray* arrayToDelete;
+@property (nonatomic)NSMutableArray* arrayImportant;
+
+
+- (IBAction)deleteTodo: (UIButton *)sender;
+- (IBAction)importantButtonA:(UIButton *)sender;
 
 
 @end
